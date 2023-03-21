edec48d4238eebd11c22510e4f4ba6537f9e30eb
Paavo Virtanen <hellothere@paavovirtanen.fi>
2023-03-21 15:48:00 +0200

    Initial commit

---
+++ ToDoApp/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+// import type { Node } from 'react';
+import {
+    ScrollView,
+    StyleSheet,
+    View,
+    Text
+} from 'react-native';
+
+
+class App extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <ScrollView keyboardShouldPersistTaps='always'
+                    style={styles.content}>
+                    <View style={styles.mainview}>
+                        <Text>Be Done With It</Text>
+                    </View>
+                </ScrollView>
+            </View>
+        )
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        //backgroundColor: '#121212'
+    },
+    content: {
+        flex: 1,
+        paddingTop: 60
+    },
+    mainview: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
+
+export default App;
